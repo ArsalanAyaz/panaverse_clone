5d7d536fd4a824272f26bec9bb1422c005624b0b
ArsalanAyaz <henrykoff7788@gmail.com>
2024-04-15 16:12:49 +0500

    image_tag

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link"
 import Wrapper from "@/components/shared/Wrapper"
+import Image from "next/image"
+import dao from "@/assets/images/dao.png"
 
 
 const Header = () => {
@@ -9,7 +11,7 @@ const Header = () => {
                 <div className='flex items-center justify-between  p-6  mt-2'>
 
                     <div>
-                        <img src="https://www.panaverse.co/red-p-logo-text_dao_croped.png" alt="panaverse dao logo" width={100} height={100}/>
+                        <Image src={dao} alt="panaverse dao logo" width={100} height={100}/>
                     </div>
 
                     <div>
